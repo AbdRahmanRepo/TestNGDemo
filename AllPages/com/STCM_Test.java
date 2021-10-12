1a5b36eaf41a989cc5f33c3fa043382549a2638b
AbdulRahman <92000559+AbdRahmanRepo@users.noreply.github.com>
2021-10-12 06:51:50 +0530

    This updated....more methods Added to it

--- AllPages/com/STCM_Test.java
+++ AllPages/com/STCM_Test.java
@@ -1,3 +1,4 @@
+//@ignore-when ignore at package level have create file as package-info.java
 package com.AllPages.com;
 
 import org.testng.annotations.AfterClass;
@@ -8,62 +9,108 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+// @Ignore - we can specify at class level also
+//@Test(groups= {"All"}) - Partial Group
 public class STCM_Test {
+	@Test(priority = 1, groups= {"smoke","functional","regresion, windows.sanity"})
+	private void testCase1() {
+		System.out.println("testCase 1 Executing, smoke,functional,regresion and Regular Expression windows.sanity");
+
+	}
+	@Test(groups= "suiteLvl")
+	private void testCase() {
+		System.out.println("testCase Group level Executing method");
+
+	}
+
+	@Test(priority = 3, groups= "smoke, ios.sanity") //ios.sanity - Regular Expression
+	private void testCase3() {
+		System.out.println("testCase 3 Executing,smoke only and Regular Expression ios.sanity");
+
+	}
+	@Test(priority = 6, groups= {"sanity","regresion, windows.smoke"})
+	private void testCase6() {
+		System.out.println("testCase 6 Executing,sanity,regresion and Regular Expression windows.smoke");
+
+	}
+	@Test(priority = 3, groups= {"smoke","functional"})
+	private void testCase7() {
+		System.out.println("testCase 7 Executing,smoke,functional");
+
+	}
+	@Test(priority = 3, groups= "functional")
+	private void testCase8() {
+		System.out.println("testCase 8 Executing, functional");
+
+	}
+	@Test(priority = 9,groups= {"smoke","regresion"})
+	private void testCase9() {
+		System.out.println("testCase 9 Executing, smoke,regresion ");
+
+	}
 	
-		@Test
-		private void testCase001() {
-			System.out.println("testCase001 Executing");
+	@Ignore			//this also Ignoring method
+	@Test(priority = 4,enabled=true )	//even method enabled...ignore has diabled method..ignore has highest precidence
+	private void testCase4() {
+		System.out.println("testCase 4 Executing");
 
-		}
-		
-		@Test
-		private void testCase002() {
-			System.out.println("testCase002 Executing");
+	}
+	@Test(priority = 5, enabled= false)		//ignore case at method level
+	private void testCase5() {
+		System.out.println("testCase 5 Executing");
 
-		}
-		
-	
-		@BeforeSuite
-		public void beforeSuit() {
-			System.out.println("beforeSuit");
+	}
 
-		}
-		@BeforeTest
-		public void beforeTest() {
-			System.out.println("beforeTest");
+	@Test(priority = 2)
+	private void testCase002() {
+		System.out.println("testCase 2 Executing");
 
-		}
-		@BeforeClass
-		private void beforeClass() {
-			System.out.println("beforeClass");
+	}
 
-		}
-		@BeforeMethod
-		private void beforeMethod() {
-			System.out.println("beforeMethod");
 
-		}
-		@AfterMethod
-		private void afterMethod() {
-			System.out.println("afterMethod");
-
-		}
-		@AfterClass
-		private void afterClass() {
-			System.out.println("afterClass");
-
-		}
-		@AfterTest
-		public void afterTest() {
-			System.out.println("afterTest");
-
-		}
-		@AfterSuite
-		public void afterSuit() {
-			System.out.println("afterSuit");
-
-		}
+//	@BeforeSuite
+//	public void beforeSuit() {
+//		System.out.println("beforeSuit");
+//
+//	}
+//	@BeforeTest
+//	public void beforeTest() {
+//		System.out.println("beforeTest");
+//
+//	}
+//	@BeforeClass
+//	private void beforeClass() {
+//		System.out.println("beforeClass");
+//
+//	}
+//	@BeforeMethod
+//	private void beforeMethod() {
+//		System.out.println("beforeMethod");
+//
+//	}
+//	@AfterMethod
+//	private void afterMethod() {
+//		System.out.println("afterMethod");
+//
+//	}
+//	@AfterClass
+//	private void afterClass() {
+//		System.out.println("afterClass");
+//
+//	}
+//	@AfterTest
+//	public void afterTest() {
+//		System.out.println("afterTest");
+//
+//	}
+//	@AfterSuite
+//	public void afterSuit() {
+//		System.out.println("afterSuit");
+//
+//	}
 
 }
+
